d13d2562f51e228187ddf58120f6f3d5bab0148f
silentscribe <seelive.i@gmail.com>
2019-01-29 21:13:18 +0000

    refactored to be more modular

--- lookup_people.js
+++ lookup_people.js
@@ -10,24 +10,34 @@ const client = new pg.Client({
   ssl      : settings.ssl
 });
 
-const userInput = process.argv.slice(2)[0];
+const data = process.argv.slice(2)[0];
 
 client.connect();
 
-client.query("SELECT * FROM famous_people WHERE first_name=$1 OR last_name=$1", [userInput], (err, res) => {
-  if (err) {
-    console.log('Error', err);
-    return false;
-  }
+function doQuery(client, query, values, cb) {
+  client.query(query, values, (err, res) => {
+    if (err) {
+      console.log('Error', err);
+      return false;
+    }
+    if (res.rows.length) {
+      console.log(`Found ${res.rows.length} person(s) by the name ${values}:`);
+      res.rows.forEach(cb);
+    } else {
+      console.log('No results found');
+    }
+    client.end();
+  });
+}
 
-  if (res.rows.length) {
-    console.log(`Found ${res.rows.length} person(s) by the name ${userInput}:`);
-    res.rows.forEach((person, index) => {
-      const date = person.birthdate.toISOString().slice(0, 10);
-      console.log(`-${index + 1}: ${person.first_name} ${person.last_name}, born '${date}'`);
-    });
-  } else {
-    console.log('No results found');
-  }
-  client.end();
-});
+function findPerson (client, data) {
+  const query = "SELECT * FROM famous_people WHERE first_name=$1 OR last_name=$1";
+  const values = [data];
+
+  doQuery(client, query, values, (person, index) => {
+    const date = person.birthdate.toISOString().slice(0, 10);
+    console.log(`-${index + 1}: ${person.first_name} ${person.last_name}, born '${date}'`);
+  });
+}
+
+// findPerson(client, data);
