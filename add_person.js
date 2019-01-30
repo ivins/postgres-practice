8d23ad68a3a5f586e6375a99ebc6663d0c59dbde
silentscribe <seelive.i@gmail.com>
2019-01-30 01:01:27 +0000

    adding person successfully

---
+++ add_person.js
@@ -0,0 +1,27 @@
+const settings = require('./settings'); // settings.json
+
+const knex = require('knex')({
+  client   : 'pg',
+  connection: {
+    user     : settings.user,
+    password : settings.password,
+    database : settings.database,
+    host     : settings.hostname,
+    port     : settings.port,
+    ssl      : settings.ssl
+  }
+});
+
+const [first, last, date] = process.argv.slice(2);
+
+function addPerson (first, last, date) {
+  knex
+    .insert({ first_name: first, last_name: last, birthdate: date })
+    .into('famous_people')
+    .asCallback((err, rows) => {
+      if (err) { console.log(err);}
+      console.log('Added successfully: ', first, last, date);
+    });
+}
+
+addPerson(first, last, date);
